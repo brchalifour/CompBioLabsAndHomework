5901594d9a4379a161a1e26bf56ac2dd43d4b267
brchalifour <brch7026@colorado.edu>
2019-01-25 12:32:44 -0700

    Added line notes to explain code

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,4 +1,8 @@
+#removes first line of PredPreyData.csv file
 grep time PredPreyData.csv
+#adds first line to new plain text file called newcutData.txt
 grep time PredPreyData.csv > newcutData.txt
+#takes out last ten lines of PredPreyData.csv file
 tail PredPreyData.csv >> newcutData.txt
+#view the new edited PredPreyData
 less newcutData.txt
