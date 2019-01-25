2f2d1a980fcf3b7cad40fa2eb08c3be93c4420b8
brchalifour <brch7026@colorado.edu>
2019-01-25 12:55:13 -0700

    fixed typo

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,6 +1,6 @@
 #move into the correct working directory to edit the PredPreyData.csv file - this will be different depending on where it is stored on your computer
 cd ~/Desktop/Grad\ School/CompBio/CompBioLabsAndHomework/Labs/Lab01
-#cuts columns 2-4 out of PredPreyData and adds first line to new file called newcutData.csv
+#cuts columns 2-4 out of PredPreyData and adds first line to new file called NewPredPrey.csv
 cut -f 2-4 -d , PredPreyData.csv | grep time PredPreyData.csv > NewPredPrey.csv
 #takes out last ten lines of PredPreyData.csv file
 cut -f 2-4 -d , PredPreyData.csv | tail PredPreyData.csv >> NewPredPrey.csv
