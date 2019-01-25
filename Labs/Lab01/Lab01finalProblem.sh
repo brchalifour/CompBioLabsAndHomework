6d4b3aa943a7414a9c3068ef6010d90f0e33561e
brchalifour <brch7026@colorado.edu>
2019-01-25 12:40:48 -0700

    changed .txt to .csv

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -2,9 +2,9 @@
 cd ~/Desktop/Grad\ School/CompBio/CompBioLabsAndHomework/Labs/Lab01
 #removes first line of PredPreyData.csv file
 grep time PredPreyData.csv
-#adds first line to new plain text file called newcutData.txt
-grep time PredPreyData.csv > newcutData.txt
+#adds first line to new file called newcutData.csv
+grep time PredPreyData.csv > newcutData.csv
 #takes out last ten lines of PredPreyData.csv file
-tail PredPreyData.csv >> newcutData.txt
+tail PredPreyData.csv >> newcutData.csv
 #view the new edited PredPreyData
-less newcutData.txt
+less newcutData.csv
