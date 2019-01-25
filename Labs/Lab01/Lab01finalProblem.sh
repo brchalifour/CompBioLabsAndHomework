404819d58a1949be9b468e257451354aa0d40949
brchalifour <brch7026@colorado.edu>
2019-01-25 12:53:19 -0700

    changed name of new ending file

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,10 +1,8 @@
 #move into the correct working directory to edit the PredPreyData.csv file - this will be different depending on where it is stored on your computer
 cd ~/Desktop/Grad\ School/CompBio/CompBioLabsAndHomework/Labs/Lab01
-#removes first line of PredPreyData.csv file
-grep time PredPreyData.csv
-#adds first line to new file called newcutData.csv
-grep time PredPreyData.csv > newcutData.csv
+#cuts columns 2-4 out of PredPreyData and adds first line to new file called newcutData.csv
+cut -f 2-4 -d , PredPreyData.csv | grep time PredPreyData.csv > NewPredPrey.csv
 #takes out last ten lines of PredPreyData.csv file
-tail PredPreyData.csv >> newcutData.csv
+cut -f 2-4 -d , PredPreyData.csv | tail PredPreyData.csv >> NewPredPrey.csv
 #view the new edited PredPreyData
-less newcutData.csv
+less NewPredPrey.csv
