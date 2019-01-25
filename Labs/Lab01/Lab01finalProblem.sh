380e8041a665dad4d56b839345d4ffa1fd3031c9
brchalifour <brch7026@colorado.edu>
2019-01-25 12:38:10 -0700

    added a cd line to the beginning

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,3 +1,5 @@
+#move into the correct working directory to edit the PredPreyData.csv file - this will be different depending on where it is stored on your computer
+cd ~/Desktop/Grad\ School/CompBio/CompBioLabsAndHomework/Labs/Lab01
 #removes first line of PredPreyData.csv file
 grep time PredPreyData.csv
 #adds first line to new plain text file called newcutData.txt
