cba727e054c1c759983fa982c393789ac07d0ff8
brchalifour <brch7026@colorado.edu>
2019-02-05 09:21:55 -0700

    Fixed with edits from Sam

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,8 +1,7 @@
 #move into the correct working directory to edit the PredPreyData.csv file - this will be different depending on where it is stored on your computer
-cd ~/Desktop/Grad\ School/CompBio/CompBioLabsAndHomework/Labs/Lab01
-#cuts columns 2-4 out of PredPreyData and adds first line to new file called NewPredPrey.csv
-cut -f 2-4 -d , PredPreyData.csv | grep time PredPreyData.csv > NewPredPrey.csv
+cd ~/CompBioLabsAndHomework/Labs/Lab01
+#cuts columns 2-4 out of PredPreyData and adds first line to new file called newcutData.csv
+cut -f 2-4 -d , PredPreyData.csv | grep time > NewPredPrey.csv
 #takes out last ten lines of PredPreyData.csv file
-cut -f 2-4 -d , PredPreyData.csv | tail PredPreyData.csv >> NewPredPrey.csv
-#view the new edited PredPreyData
-less NewPredPrey.csv
+cut -f 2-4 -d , PredPreyData.csv | tail >> NewPredPrey.csv
+
