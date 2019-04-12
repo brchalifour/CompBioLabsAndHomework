9a1c0fb119f2d86b2f83d5233487306e1e873f23
brchalifour <brch7026@colorado.edu>
2019-04-12 12:40:42 -0600

    fixed typo at end of script

--- Labs/Lab12/Chalifour_Lab12.R
+++ Labs/Lab12/Chalifour_Lab12.R
@@ -1,15 +1,18 @@
 ### Chalifour Lab 12 Script ###
 
 # Load necessary libraries 
+
 library(dplyr)
 library(ggplot2)
 
 # Attach data
+
 AnimalData <- read.csv("~/Desktop/Grad_School/CompBio/compBioSandbox/CompBio_on_git/Datasets/Cusack_et_al/Cusack_et_al_random_versus_trail_camera_trap_data_Ruaha_2013_14.csv")
 
 head(AnimalData)
 
 # Sort data and get counts per species
+
 NewAnimals <- AnimalData %>% 
   group_by(Species) %>%
   tally()
@@ -29,7 +32,7 @@ CountAnimalsCorrect
 # Problem 3 - flip the axes, sort the species from least to most abundant in the plot, 
 # and also transform the count axis to be logarithmic so that you can see the smaller count values more easily.
 
-OrderedCounts <- ggplot(NewAnimals, aes(x = reorder(Species, -n), y = n)) + geom_bar(stat = "identity") + coord_flip() + scale_y_continuous(trans='log10') + xlab("Count") + ylab("Species")
+OrderedCounts <- ggplot(NewAnimals, aes(x = reorder(Species, -n), y = n)) + geom_bar(stat = "identity") + coord_flip() + scale_y_continuous(trans='log10') + xlab("Species") + ylab("Count")
 
 OrderedCounts
 
